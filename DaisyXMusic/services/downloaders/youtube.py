8519b461a9dac309e33e088d2f781cc7371d4a6e
Technical_Hunter <85411336+Technical-Hunter@users.noreply.github.com>
2021-07-10 00:39:21 +0530

    Fix ytdl extract error

--- DaisyXMusic/services/downloaders/youtube.py
+++ DaisyXMusic/services/downloaders/youtube.py
@@ -16,6 +16,7 @@
 
 # Modified by Inukaasith
 
+
 from os import path
 
 from youtube_dl import YoutubeDL
@@ -24,7 +25,7 @@ from DaisyXMusic.config import DURATION_LIMIT
 from DaisyXMusic.helpers.errors import DurationLimitError
 
 ydl_opts = {
-    "format": "bestaudio/best",
+    "format": "bestaudio[ext=m4a]",
     "geo-bypass": True,
     "nocheckcertificate": True,
     "outtmpl": "downloads/%(id)s.%(ext)s",
@@ -39,8 +40,14 @@ def download(url: str) -> str:
 
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, " 
+            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, "
             f"the provided video is {duration} minute(s)",
-       )
-    ydl.download([url])
+        )
+    try:
+        ydl.download([url])
+    except:
+        raise DurationLimitError(
+            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, "
+            f"the provided video is {duration} minute(s)",
+        )
     return path.join("downloads", f"{info['id']}.{info['ext']}")
